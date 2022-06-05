b0d076a76fa02fa1ca3443b43d517b5070c2eaa6
Carmen Arias <maricarmen98a@gmail.com>
2022-06-05 17:33:45 +0200

    fixed issue

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -233,6 +233,7 @@ export class PaymentComponent implements OnInit {
         this.flightService.updateReservation(this.reservation, this.reservation.id).subscribe();
         console.log('se ha actualizado')
       } else {
+        this.reservation.user_id = this.UserProfile.id;
         console.log(this.reservation)
         console.log('payment this.reservation')
         this.flightService.createReservation(this.reservation).subscribe();
